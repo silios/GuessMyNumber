3eb4eff0f1d407809964c29ce098a4b705dc1d3a
Marios Tsalkidis <marios.tsalkidis@gmail.com>
2017-09-06 16:31:36 +0300

    code formating..

--- main.cpp
+++ main.cpp
@@ -6,36 +6,30 @@
 
 using namespace std;
 
-int main()
-{
-    //Seed the random number generator
-    srand(static_cast<unsigned int>(time(0)));
+int main() {
+  //Seed the random number generator
+  srand(static_cast<unsigned int>(time(0)));
 
-    //get a random number between 1-100
-    int secretNumber = rand()%100+1;
-    int tries = 0;
-    int guess;
+  //get a random number between 1-100
+  int secretNumber = rand() % 100 + 1;
+  int tries = 0;
+  int guess;
 
-    cout <<"\tWelcome to Guess My Number\n\n";
+  cout << "\tWelcome to Guess My Number\n\n";
 
-    do{
-        cout << "Enter a guess (1-100): ";
-        cin >> guess;
-        ++tries;
+  do {
+    cout << "Enter a guess (1-100): ";
+    cin >> guess;
+    ++tries;
 
-        if(guess>secretNumber)
-        {
-            cout << "Too high!\n\n";
-        }
-        else if(guess < secretNumber)
-        {
-            cout << "Too low!\n\n";
-        }
-        else
-        {
-            cout <<"\nThat's it! You got it in " << tries << " guesses!\n";
-        }
-      }while(guess != secretNumber);
-        return 0;
+    if(guess > secretNumber) {
+      cout << "Too high!\n\n";
+    } else if(guess < secretNumber) {
+      cout << "Too low!\n\n";
+    } else {
+      cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+    }
+  } while(guess != secretNumber);
+  return 0;
 }
 
