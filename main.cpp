5531263abb004d7fcbfa78d837d0e5e804975643
Marios Tsalkidis <marios.tsalkidis@gmail.com>
2017-09-06 15:49:51 +0300

    main logic finished

--- main.cpp
+++ main.cpp
@@ -8,6 +8,34 @@ using namespace std;
 
 int main()
 {
-  srand(static_cast<unsigned int>(time(0)));
+    //Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+
+    //get a random number between 1-100
+    int secretNumber = rand()%100+1;
+    int tries = 0;
+    int guess;
+
+    cout <<"\tWelcome to Guess My Number\n\n";
+
+    do{
+        cout << "Enter a guess (1-100): ";
+        cin >> guess;
+        ++tries;
+
+        if(guess>secretNumber)
+        {
+            cout << "Too high!\n\n";
+        }
+        else if(guess < secretNumber)
+        {
+            cout << "Too low!\n\n";
+        }
+        else
+        {
+            cout <<"\nThat's it! You got it in " << tries << " guesses!\n";
+        }
+      }while(guess != secretNumber);
+        return 0;
 }
 
