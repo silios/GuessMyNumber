d80b7da82d5e4911c2a4d5b77a897f369c39acaf
Marios Tsalkidis <marios.tsalkidis@gmail.com>
2017-09-06 15:33:21 +0300

    added srand

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+//GuessMyNumber - The classic number guessing game
+
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+srand(static_cast<unsigned int>(time(0)));
 }
+
